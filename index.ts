a762fe9f96a21f4791c38b349be0617677ee7942
Krishna Bansal <krishnabansal890@gmail.com>
2025-01-28 15:15:15 +0530

    FIX: Dev and Prod scripts

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import { db } from "./utils/db";
+import UserRouter from "./routes/userRoutes";
+import * as dotenv from "dotenv";
+dotenv.config({ path: '/.env' });
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -26,7 +29,7 @@ app.get("/health", (req: Request, res: Response) => {
   res.status(200).send("Server is running");
 });
 
-app.use("/user", require("./routes/user"));
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
